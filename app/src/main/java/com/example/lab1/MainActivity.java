8054d80e74746f0c3056d25671a6c46959abe165
CaleHarned <caleharned@gmail.com>
2021-01-27 16:53:51 -0600

    Incrementing Works now

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int initialNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialNum = 50;
+        String initialNumber = ""+initialNum+"";
+        TextView t = (TextView) findViewById(R.id.outputField);
+        t.setText(initialNumber);
     }
+    public void onClickIncrimentButton(View v) {
+        initialNum = initialNum + 1;
+        String initialNumber = ""+initialNum+"";
+        TextView t = (TextView) findViewById(R.id.outputField);
+        t.setText(initialNumber);
+    }
+
 }
